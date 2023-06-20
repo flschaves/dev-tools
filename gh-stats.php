ef6a181ace2a7967de427523aed662f4d6485f35
Filipe Chaves <flschaves@gmail.com>
2023-06-20 08:11:18 -0300

    First commit

---
+++ gh-stats.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Github Statistics - For developers
+ * Plugin URI:      https://flschaves.com/plugins/gh-stats
+ * Description:     This is a Github Statistics plugin for developers
+ * Author:          Filipe Chaves
+ * Author URI:      https://flschaves.com
+ * Text Domain:     gh-stats
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Gh_Stats
+ */
+
+// Your code starts here.
